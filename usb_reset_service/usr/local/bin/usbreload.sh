7e67663b7e0837f7f873a794135b90a6ba9cb899
Sam4uk <sam4uk.site@gmail.com>
2025-07-07 15:13:18 +0300

    Versio v0.1.3

--- usb_reset_service/usr/local/bin/usbreload.sh
+++ usb_reset_service/usr/local/bin/usbreload.sh
@@ -5,14 +5,12 @@ source /usr/local/etc/usb_reset_config.sh
 
 
 echo "USB reload usbreset"
-# sleep 5
+sleep 5
 
 for DEVICE in "${DEVICE_LIST[@]}"; do
-    echo "usbreset two times ${DEVICE}"
-    sleep 5
+    echo "usbreset ${DEVICE}"
     sudo usbreset ${DEVICE}
     sleep 5
-    sudo usbreset ${DEVICE}
 done
 usbreset
 
